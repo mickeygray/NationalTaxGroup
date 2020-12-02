0c81f6319e4877e0c3c5952549f77d12f6b37d9c
mickeygray <mgray@nattaxexperts.com>
2020-12-02 09:37:53 -0800

    f

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,7 @@ import {
   Switch,
   useLocation,
 } from "react-router-dom";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import PrivateRoute from "./components/routing/PrivateRoute";
+
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 import StatState from "./context/stat/StatState";
@@ -15,29 +13,18 @@ import CallState from "./context/call/CallState";
 import UserState from "./context/user/UserState";
 import "./App.css";
 import "react-datepicker/dist/react-datepicker.css";
-import ShipEm from "./components/pages/app/ShipEm";
-import Stacks from "./components/pages/app/Stacks";
-import Popkis from "./components/pages/app/Popkis";
-import Dunder from "./components/pages/app/Dunder";
-import Eyore from "./components/pages/app/Eyore";
-import TheBigGuns from "./components/pages/app/TheBigGuns";
 
-import FordeTwoCents from "./components/pages/app/FordeTwoCents";
-import DangerZone from "./components/pages/app/DangerZone";
+
+
 import LeadState from "./context/lead/LeadState";
 import EmailState from "./context/email/EmailState";
 import MailState from "./context/mail/MailState";
 import TakeMeOffTheList from "./components/pages/splash/TakeMeOffTheList";
 import FreshStart from "./components/pages/splash/FreshStart";
 import TaxGroup from "./components/pages/website/pages/TaxGroup";
-import FileAComplaint from "./components/pages/splash/FileAComplaint";
-import Criminal from "./components/pages/splash/Criminal";
-import Distraint from "./components/pages/splash/Distraint";
-import SSI from "./components/pages/splash/SSI";
+
 import LienViewer from "./components/pages/website/pages/LienViewer";
-import IGetYourMoneyBack from "./components/pages/website/pages/IGetYourMoneyBack";
-import Free1040Filler from "./components/pages/website/pages/Free1040Filler";
-import PPsPalace from "./components/pages/app/PPsPalace";
+
 const App = () => {
   return (
     <AuthState>
